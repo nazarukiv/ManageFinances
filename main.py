f20a01c63c7b64fe4988d9c94b5e4853e1bab001
Ivan Nazaruk <nazaruk7649@ukr.net>
2025-04-21 16:47:11 +0100

    load_transaction function done

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import json
-import os
 
 st.set_page_config(page_title="Manage my finances", page_icon="💷", layout="wide")
 
 
-def load_transactions(uploaded_file):
-    pass
+def load_transactions(file):
+    try:
+        df = pd.read_csv(file)
+        df.columns = [col.strip() for col in df.columns]
+        df["Amount (GBP)"] = df["Amount (GBP)"].replace(",", "", regex=True).astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], dayfirst=True, errors="coerce")
+        df["Date"] = df["Date"].dt.date
 
+        st.write(df)
+        return df
+    except Exception as e:
+        st.error(f"Error processing file: {str(e)}")
+        return None
 
 def main():
-    st.title("My Finances Dashboard")
-    uploaded_file = st.file_uploader("Upload your transaction CSV file", type = ['cvs'])
-
-    if uploaded_file is not None:
+    st.title("my finances dashboard")
+    uploaded_file = st.file_uploader("upload your transaction csv file", type=["csv"])
+    if uploaded_file:
         df = load_transactions(uploaded_file)
 
 
